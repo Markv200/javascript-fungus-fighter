446e5a7091d210681a847bd77579b3547c9560c8
Mark Velesaca <mark.velesaca@gmail.com>
2024-09-10 02:05:41 -0500

    Attacks

--- script.js
+++ script.js
@@ -18,3 +18,63 @@ function onReady() {
 
 
 onReady()
+
+
+
+let apText = document.querySelector('.ap-text');
+let apMeter = document.getElementById('ap-meter');
+let hpText = document.querySelector('.hp-text');
+let hpMeter = document.getElementById('hp-meter');
+let fungusElement = document.querySelector('.freaky-fungus');
+
+let attackButtons = document.querySelectorAll('.attack-btn');
+
+let playerAP = 100;
+let fungusHP = 100;
+
+const attacks = {
+    'arcane-scepter': { apCost: 12, damage: 14 },
+    'entangle': { apCost: 23, damage: 9 },
+    'dragon-blade': { apCost: 38, damage: 47 },
+    'star-fire': { apCost: 33, damage: 25 }
+};
+
+function attack(event) {
+    let attackType = event.target.classList[1]; 
+
+    playerAP = Math.max(playerAP - attacks[attackType].apCost, 0);
+    fungusHP = Math.max(fungusHP - attacks[attackType].damage, 0);
+
+    
+
+    renderTotalAP();
+}
+
+function renderTotalAP() {
+    apText.textContent = `${playerAP} AP`;
+    apMeter.value = playerAP;
+    hpText.textContent = `${fungusHP} HP`;
+    hpMeter.value = fungusHP;
+    
+    if (fungusHP === 0) {
+        fungusElement.classList.remove('walk');
+        fungusElement.classList.add('dead');
+        disableAttackButtons();
+        alert('You win!');
+    } else if (playerAP === 0) {
+        fungusElement.classList.remove('walk');
+        fungusElement.classList.add('jump');
+        disableAttackButtons();
+        alert('You lose!');
+    }
+}
+
+function disableAttackButtons() {
+    attackButtons.forEach(button => button.disabled = true);
+}
+
+attackButtons.forEach(button => {
+    button.addEventListener('click', attack);
+});
+
+renderTotalAP();
